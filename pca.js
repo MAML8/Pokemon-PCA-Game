18085e34eab70c48aabfd91cfa1b7145c0cf35b5
Marcos <marcanantins@gmail.com>
2025-12-10 14:44:00 -0300

    multiplos pokemon muahahaha

--- pca.js
+++ pca.js
@@ -1,61 +1,69 @@
 
-            //------------PCA-----------
-            import { Matrix, EigenvalueDecomposition } from 'https://esm.sh/ml-matrix@6.10.4';
+//------------PCA-----------
+import { Matrix, EigenvalueDecomposition } from 'https://esm.sh/ml-matrix@6.10.4';
 
-            export function matriz(a){
-                return new Matrix(a);
-            }
+export function matriz(a){
+    return new Matrix(a);
+}
 
-            export function PCA(matrizA){
-                const media = matrizA.mean('column')
-                const centralizada = matrizA.subRowVector(media);
+export function PCA(matrizA){
+    const media = matrizA.mean('column')
+    const centralizada = matrizA.subRowVector(media);
 
-                const covarianca = centralizada.transpose().mmul(centralizada);
+    const covarianca = centralizada.transpose().mmul(centralizada);
 
-                const eigs = new EigenvalueDecomposition(covarianca);
+    const eigs = new EigenvalueDecomposition(covarianca);
 
-                let autovaloresReais = eigs.realEigenvalues;
-                let autovetores = eigs.eigenvectorMatrix;
+    let autovaloresReais = eigs.realEigenvalues;
+    let autovetores = eigs.eigenvectorMatrix;
 
-                //Ordenação dos autovalores
-                let sortedIndices = autovaloresReais
-                    .map((val, idx) => ({ val, idx }))
-                    .sort((a, b) => b.val - a.val) // Decrescente
-                    .map(item => item.idx);
+    //Ordenação dos autovalores
+    let sortedIndices = autovaloresReais
+        .map((val, idx) => ({ val, idx }))
+        .sort((a, b) => b.val - a.val) // Decrescente
+        .map(item => item.idx);
 
-                // Reordenamos a matriz de autovetores baseada nos índices ordenados
-                let sortedV = new Matrix(matrizA.columns, matrizA.columns); // M x M
-                for(let j = 0; j < matrizA.columns; j++) {
-                    sortedV.setColumn(j, autovetores.getColumn(sortedIndices[j]));
-                }
+    // Reordenamos a matriz de autovetores baseada nos índices ordenados
+    let sortedV = new Matrix(matrizA.columns, matrizA.columns); // M x M
+    for(let j = 0; j < matrizA.columns; j++) {
+        sortedV.setColumn(j, autovetores.getColumn(sortedIndices[j]));
+    }
 
 
-                let autovaloresOrdenados = [];
-                for(let j = 0; j < matrizA.columns; j++) {
-                    autovaloresOrdenados[j] = autovaloresReais[sortedIndices[j]];
-                }
-                console.log(autovaloresOrdenados);
+    let autovaloresOrdenados = [];
+    let totalAutovalores = 0.0;
+    for(let j = 0; j < matrizA.columns; j++) {
+        autovaloresOrdenados[j] = autovaloresReais[sortedIndices[j]];
+        totalAutovalores += autovaloresReais[j];
+    }
+    console.log(autovaloresOrdenados);
 
-                return {
-                    media: media,
-                    matrizCentralizada: centralizada,
-                    autovalores: autovaloresOrdenados,
-                    autovetores: sortedV
-                }
-            }
+    return {
+        media: media,
+        matrizCentralizada: centralizada,
+        autovalores: autovaloresOrdenados,
+        autovetores: sortedV,
+        totalAutovalores: totalAutovalores
+    }
+}
 
-            export function reconstrucao(pcaObj, l, r){
-                // Reconstrução: A aproximado = (A_cent * V_k) * V_k^T + Media
+export function reconstrucao(pcaObj, l, r){
+    // Reconstrução: A aproximado = (A_cent * V_k) * V_k^T + Media
 
-                const Vk = pcaObj.autovetores.subMatrix(0, pcaObj.matrizCentralizada.columns - 1, l - 1, r - 1);
-                
-                const scores = pcaObj.matrizCentralizada.mmul(Vk);
+    let erro = pcaObj.totalAutovalores;
+    for(let i = l-1; i<r-1; i++)
+        erro -= pcaObj.autovalores[i];
+    console.log(erro/pcaObj.totalAutovalores*100.0);
 
-                const reconstructed = scores.mmul(Vk.transpose());
+    const Vk = pcaObj.autovetores.subMatrix(0, pcaObj.matrizCentralizada.columns - 1, l - 1, r - 1);
+    
+    const scores = pcaObj.matrizCentralizada.mmul(Vk);
 
-                return reconstructed.addRowVector(pcaObj.media);
-            }
+    const reconstructed = scores.mmul(Vk.transpose());
 
-            export function reconstrucao_melhor(pcaObj, k){
-                return reconstrucao(pcaObj, 1, k);
-            }
+    return reconstructed.addRowVector(pcaObj.media);
+}
+
+export function reconstrucao_melhor(pcaObj, k){
+    return reconstrucao(pcaObj, 1, k);
+}
